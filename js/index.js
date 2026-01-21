2a175cf78a923ce7a19c9f92a391354166e68113
Mason Ma <107304088+masonn-ma@users.noreply.github.com>
2026-01-21 23:27:29 +1100

    added sidenav

--- js/index.js
+++ js/index.js
@@ -1,15 +1,31 @@
-const THRESHOLD = 100;
-let isScrolled = false;
+// const THRESHOLD = 100;
+// let isScrolled = false;
+
+const heroSection = document.getElementById('hero');
+const sideNavbar = document.getElementById('side-navbar');
+
+const observer = new IntersectionObserver((entries) => {
+    const entry = entries[0];
+    if (entry.isIntersecting) {
+        sideNavbar.classList.remove('visible');
+    } else if (!entry.isIntersecting) {
+        sideNavbar.classList.add('visible');
+    }
+}, { threshold: 0.1 });
+
 
 function onScroll() {
-    if (!isScrolled && window.scrollY > THRESHOLD) {
-        isScrolled = true;
-        navbar.classList.add("scrolled");
-    } else if (isScrolled && window.scrollY < THRESHOLD - 10) {
-        isScrolled = false;
-        navbar.classList.remove("scrolled");
-    }
+    // if (!isScrolled && window.scrollY > THRESHOLD) {
+    //     isScrolled = true;
+    //     navbar.classList.add("scrolled");
+    // } else if (isScrolled && window.scrollY < THRESHOLD - 10) {
+    //     isScrolled = false;
+    //     navbar.classList.remove("scrolled");
+    // }
+
+
 }
 
+observer.observe(heroSection);
 window.addEventListener('scroll', onScroll, { passive: true });
 onScroll();
