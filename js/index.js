b427e618cffa5185bb0a5c24d3005b37bff7b772
Mason Ma <107304088+masonn-ma@users.noreply.github.com>
2026-03-02 21:40:15 +0700

    color scheme changes, moved sidenav, removed navbar

--- js/index.js
+++ js/index.js
@@ -6,25 +6,13 @@ const sideNavbar = document.getElementById('side-navbar');
 
 const observer = new IntersectionObserver((entries) => {
     const entry = entries[0];
-    if (entry.isIntersecting) {
+    const sideNavbarVisible = sideNavbar.classList.contains('visible');
+    if (entry.isIntersecting && sideNavbarVisible) {
         sideNavbar.classList.remove('visible');
-    } else if (!entry.isIntersecting) {
+    } else if (!entry.isIntersecting && !sideNavbarVisible) {
         sideNavbar.classList.add('visible');
     }
-}, { threshold: 0.1 });
-
-
-function onScroll() {
-    // if (!isScrolled && window.scrollY > THRESHOLD) {
-    //     isScrolled = true;
-    //     navbar.classList.add("scrolled");
-    // } else if (isScrolled && window.scrollY < THRESHOLD - 10) {
-    //     isScrolled = false;
-    //     navbar.classList.remove("scrolled");
-    // }
-
-
-}
+}, { threshold: 0.5 });
 
 observer.observe(heroSection);
 window.addEventListener('scroll', onScroll, { passive: true });
