3d30981e88db3e13aa05627012f23d1a1943a90d
Mason Ma <107304088+masonn-ma@users.noreply.github.com>
2026-03-30 16:54:13 +0700

    added horizontal list to showcase images

--- mason_ma_portfolio/lib/main.dart
+++ mason_ma_portfolio/lib/main.dart
@@ -11,10 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Portfolio',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        useMaterial3: true,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
       home: const HomePage(),
     );
   }
@@ -33,27 +30,28 @@ class HomePage extends StatelessWidget {
         iconTheme: const IconThemeData(color: Colors.black),
         titleTextStyle: const TextStyle(color: Colors.black, fontSize: 20),
       ),
-      body: Center(
-        child: Container(
-          width: 500,
-          height: 350,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(16),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.3),
-                blurRadius: 10,
-                offset: const Offset(0, 5),
-              ),
-            ],
-          ),
-          child: Center(
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 700,
+                  margin: const EdgeInsets.all(24),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        blurRadius: 10,
+                        offset: const Offset(0, 5),
+                      ),
+                    ],
+                  ),
+                  child: Padding(
                     padding: const EdgeInsets.all(24.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -79,11 +77,63 @@ class HomePage extends StatelessWidget {
                       ],
                     ),
                   ),
-                ],
-              ),
+                ),
+                const SizedBox(height: 24),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: HorizontalList(),
+                ),
+              ],
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class HorizontalList extends StatelessWidget {
+  const HorizontalList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> imagePaths = [
+      'assets/images/project1.jpg',
+      'assets/images/project2.jpg',
+      'assets/images/project3.jpg',
+      'assets/images/project4.jpg',
+      'assets/images/project5.jpg',
+    ];
+
+    return SizedBox(
+      height: 300,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          for (final imagePath in imagePaths)
+            Container(
+              width: MediaQuery.of(context).size.width / 2,
+              margin: const EdgeInsets.symmetric(horizontal: 0),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(0)),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(0),
+                child: Image.asset(
+                  imagePath,
+                  fit: BoxFit.cover,
+                  frameBuilder:
+                      (context, child, frame, wasSynchronouslyLoaded) {
+                        if (wasSynchronouslyLoaded) return child;
+
+                        return AnimatedOpacity(
+                          opacity: frame == null ? 0 : 1,
+                          duration: const Duration(milliseconds: 500),
+                          child: child,
+                        );
+                      },
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
